f9588c3a16676c0dec6909bef58605339c9eb9df
spornraftjulia <julia_ramosraft@hotmail.com>
2022-11-18 17:33:29 +0000

    Connection Class

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 require_once './vendor/autoload.php';
-use PDO;
 
-$bd = new PDO('mysql:host=localhost;dbname=biblioteca', 'root', '');
+use ExemploPDOMySQL\MySQLConnection; //PDO
+
+$bd = new MySQLConnection(); //PDO('mysql:host=localhost;dbname=biblioteca', 'root', '');
 
 $comando = $bd->prepare('SELECT * FROM generos');
 $comando->execute();
+
 $generos = $comando->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
